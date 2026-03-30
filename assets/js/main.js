4b2957207e4cfa3e9dc31db59e5bbab3f3a2a28e
Anng <grilledog296@gmail.com>
2026-03-30 20:44:29 +0700

    Update tawkto live chat

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,7 +2,6 @@ import { initNavbar } from "./modules/navbar.js";
 import { coursesData, renderCourses, loadMoreCourses } from "./modules/courses.js";
 import { initSidebar } from "./modules/sidebar.js";
 import { initSearch } from "./modules/search.js";
-import { initChatWidget } from "./modules/chat.js";
 import { initCounter } from "./modules/counter.js";
 import { initThemeToggle } from "./modules/theme.js";
 import { initCart } from "./modules/cart.js";
